301c4fe2c5ac4fab773ef83ddfc680f97e11975f
Dylan Wyrick <xpoweroutage@gmail.com>
2023-09-14 15:24:54 +0000

    alarm clock without sound

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 // initalize alarm clock
 const display = document.getElementById('clock'); 
 
+const audio = new Audio()
+let alarmTime = null;
+let alarmTimeout = null;
+
 // defines a function called time
 function time() {
   let d = new Date(); // // Create a new Date object to get the current date and time
@@ -12,7 +16,34 @@ function time() {
   display.textContent = h + ":" + m + ":" + s; // Update the text content of the 'display' element with the current time
 }
 
+function setAlarmTime(value) {
+  alarmTime = value;
+}
+
+function setAlarm() {
+  if (alarmTime) {
+    const current = new Date();
+    const timeUntilAlarm = new Date(alarmTime);
+
+    if (timeUntilAlarm > current) {
+      const timeout = timeUntilAlarm.getTime() - current.getTime();
+      alarmTimeout = setTimeout(() => audio.play(), timeout);
+      alert('Set Alarm')
+    }
+  }
+}
+
+function clearAlarm() {
+  audio.pause;
+  if (alarmTimeout) {
+    clearTimeout(alarmTimeout);
+    alert('Alarm Cleared')
+  }
+}
+
+
 setInterval(time, 1000); // Call the 'time' function every 1000 milliseconds (1 second)
+
 
 
 /*/ ⬇ changes from a 12-hour and a 24-hour display on a button press ⬇ /*/
